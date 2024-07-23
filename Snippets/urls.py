0382b931f5fd872f61c51577f3e8356ae4b14a92
Nikolay Frolov <frolovne70@gmail.com>
2024-07-23 12:39:35 +0300

    Done. M5:task1-10

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
+2
 
 urlpatterns = [
-    path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('', views.index_page, name="home"),
+    path('snippets/add', views.add_snippet_page, name="snippets-add"),
+    path('snippets/list', views.snippets_page, name="snippets-list"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
