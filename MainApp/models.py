6f1399cbaa0cfc8eb3f01630433610800f60cc21
Nikolay Frolov <frolovne70@gmail.com>
2024-07-24 15:01:36 +0300

    Add user field to Snippet

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 LANGS = (
     ("py", 'Python'),
@@ -7,8 +9,10 @@ LANGS = (
     ("html", "HTML")
 )
 
+
 class Snippet(models.Model):
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
     creation_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, blank=True, null=True)
