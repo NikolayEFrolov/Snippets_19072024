f4fc936ea1de8914d51f59ba8f84f7236cacfe17
Nikolay Frolov <frolovne70@gmail.com>
2024-07-25 20:33:07 +0300

    error

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,8 @@
-from django.http import Http404, HttpResponse, HttpResponseNotFound
+from django.http import Http404, HttpResponse, HttpResponseNotAllowed, HttpResponseNotFound
 from django.shortcuts import render, redirect, get_object_or_404
 from MainApp.models import Snippet
 from django.core.exceptions import ObjectDoesNotExist
-from MainApp.forms import SnippetForm
+from MainApp.forms import SnippetForm, UserRegistrationForm, CommentForm
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 
@@ -31,7 +31,7 @@ def add_snippet_page(request):
             }
         return render(request, 'pages/add_snippet.html', context)
     
-    # Получаем данные из формы и на их основе создаем новый Сниппет в БД
+    # Получаем данные из формы и на их основе создаем нового пользователя в БД
     if request.method == "POST":
         form = SnippetForm(request.POST)
         if form.is_valid():
@@ -59,8 +59,10 @@ def snippet_detail(request, snippet_id):
     except ObjectDoesNotExist:
         return render(request, "pages/errors.html", context | {"error": f"Snippet with id={snippet_id} not found"})
     else:
+        comments_form = CommentForm()
         context["snippet"] = snippet
         context["type"] = "view"
+        context["comments_form"] = comments_form
         return render(request, "pages/snippet_detail.html", context)
 
 
@@ -122,9 +124,41 @@ def login(request):
             return render(request, "pages/index.html", context)
     return redirect('home')
 
-def auth_login(request):
-    return render(request, "pages/login.html")
 
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+def create_user(request):
+    context = {'pagename': 'Регистрация нового пользователя'}
+    # Создаем пустую форму при запросе GET
+    if request.method == "GET":
+        form = UserRegistrationForm()
+        context["form"] = form
+        return render(request, 'pages/registration.html', context)
+    
+    # Получаем данные из формы и на их основе создаем новый Сниппет в БД
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        context["form"] = form
+        return render(request,'pages/registration.html', {'form': form})
+    
+    return HttpResponseNotAllowed(["GET", "POST"])
+
+
+@login_required
+def comments_add(request):
+    if request.method == "POST":
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            snippet_id = request.POST.get("snippet_id")
+            snippet = Snippet.objects.get(id=snippet_id)
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.snippet = snippet
+            comment.save()
+            return redirect("snippet-detail", snippet_id=snippet.id)
+    return HttpResponseNotAllowed(["POST"])
